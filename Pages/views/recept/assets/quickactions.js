3239ce852e0cd4792e9b6913852fcfac82676372
sgtgiggsy <sgtgiggsy@gmail.com>
2026-05-12 22:36:12 +0200

    Recept egyklikkes Szakácskönyvbementése a UI-ról JS-sel

---
+++ Pages/views/recept/assets/quickactions.js
@@ -0,0 +1,47 @@
+let bookmarks= document.querySelectorAll(".bookmark");
+let csillagok = document.querySelectorAll(".csillag");
+bookmarks.forEach(bookmark => {
+    bookmark.addEventListener("click",function(e){
+        e.preventDefault();
+        const receptId = bookmark.getAttribute("data-receptid");
+        const formData = new FormData();
+
+        formData.append("recept_id", receptId);
+        formData.append("csrf_token", csrf_token);
+
+        fetch(RootPath + "/api/recept/bookmark", {
+            method: "POST",
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            let icon = 'success'
+            let message = 'Recept sikeresen mentve a Szakácskönyvbe'
+            let title = 'Siker'
+            if(!data.success) {
+                icon = 'error'
+                message = data.error.message
+                title = 'Hiba'
+            }
+            toaster.fire({
+                title: title,
+                text: message,
+                icon: icon
+            });
+        })
+        .catch(error => {
+            toaster.fire({
+                title: "Hiba",
+                text: error,
+                icon: "error"
+            });
+        });
+    })
+});
+
+csillagok.forEach(csillag => {
+    csillag.addEventListener("click", function (e) {
+        e.preventDefault();
+        console.log(csillag.getAttribute("data-ertek"))
+    })
+});
