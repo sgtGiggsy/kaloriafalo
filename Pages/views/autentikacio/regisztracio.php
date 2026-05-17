3586790254231a1e31eace7f1497207b00da0e15
sgtgiggsy <sgtgiggsy@gmail.com>
2026-05-17 16:57:19 +0200

    Jelszóvalidáció bővebb vizuális elemeinek hozzáadása a regisztráció nézethez

--- Pages/views/autentikacio/regisztracio.php
+++ Pages/views/autentikacio/regisztracio.php
@@ -24,25 +24,40 @@ if (!defined('ROOT_PATH')) {
                 placeholder="email@cim.hu"
                 id="email"
                 autocomplete="email"
-                required></input>
-        </div>
-        <div>
-            <label for="jelszo">Jelszó</label>
-            <input type="password"
-                name="jelszo"
-                placeholder="Jelszó"
-                id="jelszo1"
-                autocomplete="new-password"
                 required>
         </div>
-        <div>
-            <label for="jelszoismetles">Jelszó megerősítés</label>
-            <input type="password"
-                name="jelszoismetles"
-                placeholder="Jelszó megerősítés"
-                id="jelszoismetles"
-                autocomplete="new-password"
-                required>
+        <div class="jelszoblokk">
+            <div>
+                <label for="jelszo">Jelszó</label>
+                <input type="password"
+                    name="jelszo"
+                    placeholder="Jelszó"
+                    id="jelszo"
+                    autocomplete="new-password"
+                    required>
+            </div>
+            <div>
+                <label for="jelszoismetles">Jelszó megerősítés</label>
+                <input type="password"
+                    name="jelszoismetles"
+                    placeholder="Jelszó megerősítés"
+                    id="jelszoismetles"
+                    autocomplete="new-password"
+                    required>
+            </div>
+            <div>
+                <ul id="jelszoszabalyok">
+                    <li id="rule-length">Legalább 12 karakter</li>
+                    <li id="rule-lower">Kisbetű</li>
+                    <li id="rule-upper">Nagybetű</li>
+                    <li id="rule-number">Szám</li>
+                    <li id="rule-match">Jelszavak egyeznek</li>
+                </ul>
+                <div id="jelszoerobox">
+                    <div id="jelszocsik"></div>
+                    <div id="jelszoero">Jelszó erősség: -</div>
+                </div>
+            </div>
         </div>
         <div class="customcbwrapper top">
             <label class="customcb">
